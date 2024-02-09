0616f2726ed936063e14ba81e5ec8a08b760b228
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-02-08 20:14:46 -0700

    Created functionality allowing a new cover to be created based on user input on via a form

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 //<><>querySelectors<><>
+
 var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var coverTagLine1 = document.querySelector('.tagline-1');
@@ -9,10 +10,16 @@ var buttonRandomCover = document.querySelector('.random-cover-button');
 var buttonSaveCover = document.querySelector('.save-cover-button');
 var buttonViewSaved = document.querySelector('.view-saved-button');
 var buttonMakeOwn = document.querySelector('.make-new-button');
+var buttonCreateBook = document.querySelector('.create-new-book-button');
 
 var viewHome = document.querySelector('.home-view');
 var viewMakeOwn = document.querySelector('.form-view');
 var viewViewSaved = document.querySelector('.saved-view');
+
+var coverInput = document.querySelector('#cover');
+var titleInput = document.querySelector('#title');
+var firstDescriptorInput = document.querySelector('#descriptor1');
+var secondDescriptorInput = document.querySelector('#descriptor2');
 
 //<><>variables<><>
 
@@ -28,6 +35,9 @@ buttonRandomCover.addEventListener('click', init);
 buttonHome.addEventListener('click', setHome);
 buttonMakeOwn.addEventListener('click', setMake);
 buttonViewSaved.addEventListener('click', setSaved);
+buttonCreateBook.addEventListener('click', saveCover);
+buttonCreateBook.addEventListener('click', saveCover);
+
 
 //<><>event handler functions<><>
 
@@ -35,6 +45,29 @@ function init() {
   currentCover = randomCover();
   updateCover();
 };
+
+function saveCover(e) {
+  covers.push(coverInput.value);
+  titles.push(titleInput.value);
+  descriptors.push(firstDescriptorInput.value);
+  descriptors.push(secondDescriptorInput.value);
+
+  currentCover = createCover(
+    coverInput.value, 
+    titleInput.value, 
+    firstDescriptorInput.value, 
+    secondDescriptorInput.value
+    );
+  updateCover();
+  setHome();
+  
+  coverInput.value = '';
+  titleInput.value = '';
+  descriptor1.value = '';
+  descriptor2.value = '';
+
+  e.preventDefault();
+}
 
 function updateCover() {
   coverImage.src = currentCover.coverImg;
@@ -102,3 +135,9 @@ function randomCover() {
 }
 
 init();
+
+// should fill out inputs value into fields
+// cover is made based on input values
+// save submitted info into proper arrays
+// setHome button to hide
+// create new cover using inputted values
